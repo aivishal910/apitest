66ad42a70cf9e24f5471e1bbf9d6526f22940093
aivishal910 <aivishwarya@gmail.com>
2025-07-16 14:02:25 +0530

    Update app.py
    free

--- app.py
+++ app.py
@@ -29,3 +29,7 @@ def translate_text(request: TranslationRequest):
 
     except Exception as e:
         return {"error": str(e)} 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
